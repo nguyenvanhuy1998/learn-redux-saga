5c87e4fcc584e18a505c22675a3c6b042f080652
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-07-04 14:06:25 +0700

    Add Material UI Library to reactjs typescript

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,7 +1,33 @@
-import * as React from 'react';
+import { Box, Button, makeStyles, Paper, Typography, useTheme } from '@material-ui/core';
 
-export interface LoginPageProps {}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+  },
+  box: {
+    padding: theme.spacing(3),
+  },
+  button: {
+    marginTop: '32px',
+  },
+}));
 
-export default function LoginPage(props: LoginPageProps) {
-  return <div>LoginPage</div>;
+export default function LoginPage() {
+  const classes = useStyles(useTheme());
+  return (
+    <div className={classes.root}>
+      <Paper elevation={1} className={classes.box}>
+        <Typography variant="h5">Student Management</Typography>
+        <Box className={classes.button}>
+          <Button fullWidth variant="contained" color="primary">
+            Fake Login
+          </Button>
+        </Box>
+      </Paper>
+    </div>
+  );
 }
